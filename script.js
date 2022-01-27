275824cca7eb458e294250221afa21e3a53e36a9
Atlefar <atle.carlsen@live.no>
2022-01-27 12:53:44 +0100

    ferdig

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
+const imgContainer = document.getElementById("img-container");
+const buttonShow = document.getElementById("buttonShow");
+const buttonReset = document.getElementById("buttonReset");
+const resultContainer = document.getElementById("result-container");
 let resultat = "";
 
-function stjerne() {
-  resultat += "stjerne";
-  document.getElementById("resultat").innerHTML = resultat;
+function selectItems(clickedElement) {
+  resultat += clickedElement.innerHTML;
 }
 
-function boble() {
-  resultat += boble;
-  document.getElementById("resultat").innerHTML = resultat;
+function displayItems() {
+  resultContainer.innerHTML += resultat;
+  resultat = "";
+}
+
+function resetItems() {
+  resultContainer.innerHTML = "";
 }
